8478c3b3175c2703eb32cc124c578635c71eefff
Bob Mulder <5465074+bobmulder@users.noreply.github.com>
2021-01-12 20:26:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ module.exports = function () {
     enforce: 'pre',
 
     async load (id) {
-      if (id.endsWith('.svg')) {
-        const svg = await fs.readFile(id, 'utf-8')
-
-        const { code } = compileTemplate({
-          id: JSON.stringify(id),
-          source: svg,
-          transformAssetUrls: false
-        })
-
-        return `${code}\nexport default render`
+      if (! id.endsWith('.svg')) {
+        return null; 
       }
 
-      return null
+      const svg = await fs.readFile(id, 'utf-8')
+
+      const { code } = compileTemplate({
+        id: JSON.stringify(id),
+        source: svg,
+        transformAssetUrls: false
+      })
+
+      return `${code}\nexport default render`
     }
   }
 }
