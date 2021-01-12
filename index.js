9f6ff9e3d223ce7da2711b52d08846283d58d968
Jan-Paul Kleemans <jpkleemans@gmail.com>
2021-01-12 17:11:26 +0100

    Use require instead of import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { promises as fs } from 'fs'
-import { compileTemplate } from '@vue/compiler-sfc'
+const fs = require('fs').promises
+const { compileTemplate } = require('@vue/compiler-sfc')
 
 export default function () {
   return {
