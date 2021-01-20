f38ce6e1be75098d71d5be07e3edbd6093453c3b
Luciano Felix <felix.lucianodias@gmail.com>
2021-01-20 10:56:58 -0300

    Split query from path

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const { extname } = require("path")
 const fs = require('fs').promises
 const { compileTemplate } = require('@vue/compiler-sfc')
 
-module.exports = function () {
+export default function () {
   return {
     name: 'svg-loader',
     enforce: 'pre',
 
     async load (id) {
-      if (!extname(id).startsWith('.svg')) {
+      const path = id.split("?")[0]
+
+      if (!extname(path).startsWith('.svg')) {
         return null
       }
-
-      const svg = await fs.readFile(id, 'utf-8')
+      
+      const svg = await fs.readFile(path, 'utf-8')
 
       const { code } = compileTemplate({
         id: JSON.stringify(id),
