b3a3e3a0da9c84bc8028ca86a406d415eb2b68d5
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2021-01-28 14:19:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { extname } = require("path")
 const fs = require('fs').promises
 const { compileTemplate } = require('@vue/compiler-sfc')
 
-export default function () {
+module.exports = function () {
   return {
     name: 'svg-loader',
     enforce: 'pre',
