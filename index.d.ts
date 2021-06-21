6ca112d0d9de56756d8de87e5ace1a36256a021a
falstack <icesilt@outlook.com>
2021-06-21 10:34:56 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module 'vite-svg-loader' {
   import { Plugin } from 'vite'
-  function svgLoader(options?: { svgoConfig?: Object }): Plugin
+  function svgLoader(options?: { svgoConfig?: Object, disabledSvgo?: boolean }): Plugin
   export default svgLoader
 }
 
