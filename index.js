eac3477970f230257954e78e4c851458b1613685
Thy3634 <uipi@foxmail.com>
2021-07-31 14:50:42 +0800

    linter over now

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ const fs = require('fs').promises
 const { compileTemplate } = require('@vue/compiler-sfc')
 const { optimize: optimizeSvg } = require('svgo')
 
-module.exports = function svgLoader(options = {}) {
+module.exports = function svgLoader (options = {}) {
   const { svgoConfig, svgo } = options
 
   const svgRegex = /\.svg(\?(raw|url|component))?$/
   return {
     name: 'svg-loader',
     enforce: 'pre',
-    resolveid() {
+    resolveid (id) {
       return id.match(svgRegex) ? id : null
     },
-    async load(id) {
+    async load (id) {
       if (!id.match(svgRegex)) {
         return
       }
-      const [path, query] = id.split("?", 2)
-      if (query == "url") {
+      const [path, query] = id.split('?', 2)
+      if (query === 'url') {
         return path
-      } else if (query == "raw") {
+      } else if (query === 'raw') {
         return await fs.readFile(path, 'utf-8')
       }
     },
-    async transform(src, id) {
+    async transform (src, id) {
       if (!id.match(svgRegex)) {
         return
       }
-      const [path, query] = id.split("?", 2)
-      if (query == "component") {
+      const [path, query] = id.split('?', 2)
+      if (query === 'component') {
         const svg = await fs.readFile(path, 'utf-8')
         const optimizedSvg = svgo === false ? svg : optimizeSvg(svg, svgoConfig).data
 
@@ -36,9 +36,9 @@ module.exports = function svgLoader(options = {}) {
           id: JSON.stringify(id),
           source: optimizedSvg,
           filename: path,
-          transformAssetUrls: false,
-        });
-        code = code.replace('export function render', 'function render');
+          transformAssetUrls: false
+        })
+        code = code.replace('export function render', 'function render')
         return { code: `${code}\nexport default render`, ast, map }
       }
       return `export default ${JSON.stringify(src)}`
