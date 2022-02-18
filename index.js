f48f74c7fce8dda751c3798dd07f4068526418b1
AndersLauridsen <98959184+AndersLauridsen@users.noreply.github.com>
2022-02-18 08:59:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const fs = require("fs").promises;
-const { compileTemplate } = require("@vue/compiler-sfc");
-const { optimize: optimizeSvg } = require("svgo");
+const fs = require('fs').promises
+const { compileTemplate } = require('@vue/compiler-sfc')
+const { optimize: optimizeSvg } = require('svgo')
 
 module.exports = function svgLoader(options = {}) {
-  const { svgoConfig, svgo } = options;
+  const { svgoConfig, svgo } = options
 
-  const svgRegex = /\.svg(\?(raw|component))?$/;
+  const svgRegex = /\.svg(\?(raw|component))?$/
 
   return {
-    name: "svg-loader",
-    enforce: "pre",
+    name: 'svg-loader',
+    enforce: 'pre',
 
-    resolveid(id) {
+    resolveid (id) {
       if (id.match(svgRegex)) {
-        return id;
+        return id
       }
     },
 
-    async load(id) {
+    async load (id) {
       if (!id.match(svgRegex)) {
-        return;
+        return
       }
 
-      const [path, query] = id.split("?", 2);
+      const [path, query] = id.split('?', 2)
 
-      let svg = await fs.readFile(path, "utf-8");
+      let svg = await fs.readFile(path, 'utf-8')
 
-      if (query === "raw") {
-        return `export default ${JSON.stringify(svg)}`;
+      if (query === 'raw') {
+        return `export default ${JSON.stringify(svg)}`
       }
 
-      let deletePath = false;
+      let deletePath = false
       if (svgoConfig && !svgoConfig.path) {
-        svgoConfig.path = path;
-        deletePath = true;
+        svgoConfig.path = path
+        deletePath = true
       }
 
       if (svgo !== false) {
-        svg = optimizeSvg(svg, svgoConfig).data;
+        svg = optimizeSvg(svg, svgoConfig).data
       }
 
       const { code } = compileTemplate({
@@ -45,16 +45,16 @@ module.exports = function svgLoader(options = {}) {
         source: svg,
         filename: path,
         transformAssetUrls: false
-      });
+      })
 
       if (deletePath) {
-        delete svgoConfig.path;
+        delete svgoConfig.path
       }
 
-      return `${code}\nexport default { render: render }`;
+      return `${code}\nexport default { render: render }`
     }
-  };
-};
+  }
+}
 
-module.exports.default = module.exports;
+module.exports.default = module.exports
 
