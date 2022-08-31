30c6d0490cc98b9614deed94e42f19edcb174b04
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2022-08-31 16:35:29 -0400

    [FIX] Extend svgo defaults

--- examples/vue/vite.config.js
+++ examples/vue/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
             defaultImport: DEFAULT_IMPORT,
             svgoConfig: {
                 plugins: [
+                    'preset-default',
                     { name: 'prefixIds' },
                 ],
             }
