b8e40815fc907c50c9a79ba1b250cdf496d05759
Jan-Paul Kleemans <jpkleemans@gmail.com>
2022-09-01 15:20:56 +0200

    Fix Typescript error

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,12 @@
-import { Plugin } from 'vite'
-import { OptimizeOptions } from 'svgo'
-import { FunctionalComponent, SVGAttributes } from 'vue'
-
 declare module 'vite-svg-loader' {
+  import { Plugin } from 'vite'
+  import { OptimizeOptions } from 'svgo'
   function svgLoader(options?: { svgoConfig?: OptimizeOptions, svgo?: boolean, defaultImport?: 'url' | 'raw' | 'component' }): Plugin
   export default svgLoader
 }
 
 declare module '*.svg?component' {
+  import { FunctionalComponent, SVGAttributes } from 'vue'
   const src: FunctionalComponent<SVGAttributes>
   export default src
 }
@@ -23,6 +22,7 @@ declare module '*.svg?raw' {
 }
 
 declare module '*.svg?skipsvgo' {
+  import { FunctionalComponent, SVGAttributes } from 'vue'
   const src: FunctionalComponent<SVGAttributes>
   export default src
 }
