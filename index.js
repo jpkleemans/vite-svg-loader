dbcfc5790fa9fe049a57c3623bed0f1ee821526c
where where <qq2518766683@outlook.com>
2025-11-23 21:46:23 +0800

    Only use first top title as default value

--- index.js
+++ index.js
@@ -51,13 +51,16 @@ module.exports = function svgLoader (options = {}) {
       // To prevent compileTemplate from removing the style tag
       svg = svg.replace(/<style/g, '<component is="style"').replace(/<\/style/g, '</component')
 
-      let defaultTitle = 'undefined'
-      const titleMatch = svg.match(/<title[^>]*>([\s\S]*?)<\/title>/i)
-      if (titleMatch) {
-        defaultTitle = JSON.stringify(titleMatch[1].trim())
-        svg = svg.replace(/<title([^>]*)>[\s\S]*?<\/title>/i, (_, attrs) => `<title${attrs} v-if="title">{{ title }}</title>`)
-      } else {
-        svg = svg.replace(/<svg\b([^>]*)>/i, m => `${m}<title v-if="title">{{ title }}</title>`)
+      let defaultTitle = "''"
+      const svgMatch = svg.match(/<svg\b[^>]*>([\s\S]*?)<\/svg>/i)
+      if (svgMatch) {
+        const titleMatch = svgMatch[1].match(/^\s*<title[^>]*>([\s\S]*?)<\/title>/i)
+        if (titleMatch) {
+          defaultTitle = JSON.stringify(titleMatch[1].trim())
+          svg = svg.replace(/<title([^>]*)>[\s\S]*?<\/title>/i, (_, attrs) => `<title${attrs} v-if="title">{{ title }}</title>`)
+        } else {
+          svg = svg.replace(/<svg\b([^>]*)>/i, m => `${m}<title v-if="title">{{ title }}</title>`)
+        }
       }
 
       const { code, map } = compileTemplate({
