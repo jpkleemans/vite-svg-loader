6684e91d05222911cbc0f6d374ae2a1f311171f3
Jan-Paul Kleemans <jpkleemans@gmail.com>
2026-02-05 16:12:54 +0100

    Move test files to fixtures folder

--- create-test-app.sh
+++ create-test-app.sh
@@ -3,13 +3,13 @@
 $1 # Install vue command, project name should be `test-app`
 
 # Copy example code and SVGs
-cp ./example/src/App.vue ./test-app/src/App.vue
-cp ./example/vite.config.ts ./test-app/vite.config.ts
-cp ./example/src/vite-env.d.ts ./test-app/src/vite-env.d.ts
-cp ./example/src/assets/test.svg ./test-app/src/assets/test.svg
-cp ./example/src/assets/style.svg ./test-app/src/assets/style.svg
-cp ./example/src/assets/circle.svg ./test-app/src/assets/circle.svg
-cp ./example/public/root.svg ./test-app/public/root.svg
+cp ./fixtures/App.vue ./test-app/src/App.vue
+cp ./fixtures/vite.config.ts ./test-app/vite.config.ts
+cp ./fixtures/vite-env.d.ts ./test-app/src/vite-env.d.ts
+cp ./fixtures/test.svg ./test-app/src/assets/test.svg
+cp ./fixtures/style.svg ./test-app/src/assets/style.svg
+cp ./fixtures/circle.svg ./test-app/src/assets/circle.svg
+cp ./fixtures/root.svg ./test-app/public/root.svg
 
 # Commands when not using typescript
 if [[ "$2" != "--typescript" ]]; then
